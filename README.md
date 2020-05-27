531653d633774f1bdc1f82922740756baa7b39ee
Paul Baumgarten <pbaumgarten@gmail.com>
2020-05-27 16:01:05 +0800

    initial

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # arduino-led-matrix
- Using an 16x32 LED matrix for a digital sign
+Using an 16x32 LED matrix for a digital sign
+
+Currently based on the example text scroller code provided by Adafruit.
+
+Intention is to add a wifi module so the text can be set via a remote application.
+
+## Requirements
+
+In your Arduino IDE / Manage Libraries, install the following:
+
+* Adafruit GFX Library by Adafruit
+* RGB maxtrix Panel by Adafruit
+
+## Wiring
+
+Looking at the rear of the matrix, 6there is a ribbon cable connector set of pins (2 columns of 9 pins). This was the wiring my matrix required:
+
+* Left 0 to Arduino GND
+* Left 1 to none
+* Left 2 to none
+* Left 3 to Arduino A1
+* Left 4 to Arduino D2
+* Left 5 to Arduino D3
+* Left 6 to Arduino D4
+* Left 7 to Arduino D5
+* Left 8 to Arduino D6
+* Right 0 to Arduino D8
+* Right 1 to Arduino D10
+* Right 2 to Arduino D9
+* Right 3 to Arduino A2
+* Right 4 to Arduino A0
+* Right 5
+* Right 6
+* Right 7 to Arduino D7
+
---
+++ text_scroller/text_scroller.ino
@@ -0,0 +1,70 @@
+// scrolltext demo for Adafruit RGBmatrixPanel library.
+// Demonstrates double-buffered animation on our 16x32 RGB LED matrix:
+// http://www.adafruit.com/products/420
+
+// Written by Limor Fried/Ladyada & Phil Burgess/PaintYourDragon
+// for Adafruit Industries.
+// BSD license, all text above must be included in any redistribution.
+
+#include <RGBmatrixPanel.h>
+
+// Most of the signal pins are configurable, but the CLK pin has some
+// special constraints.  On 8-bit AVR boards it must be on PORTB...
+// Pin 8 works on the Arduino Uno & compatibles (e.g. Adafruit Metro),
+// Pin 11 works on the Arduino Mega.  On 32-bit SAMD boards it must be
+// on the same PORT as the RGB data pins (D2-D7)...
+// Pin 8 works on the Adafruit Metro M0 or Arduino Zero,
+// Pin A4 works on the Adafruit Metro M4 (if using the Adafruit RGB
+// Matrix Shield, cut trace between CLK pads and run a wire to A4).
+
+#define CLK  8   // USE THIS ON ARDUINO UNO, ADAFRUIT METRO M0, etc.
+//#define CLK A4 // USE THIS ON METRO M4 (not M0)
+//#define CLK 11 // USE THIS ON ARDUINO MEGA
+#define OE   9
+#define LAT 10
+#define A   A0
+#define B   A1
+#define C   A2
+
+// Last parameter = 'true' enables double-buffering, for flicker-free,
+// buttery smooth animation.  Note that NOTHING WILL SHOW ON THE DISPLAY
+// until the first call to swapBuffers().  This is normal.
+RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
+// Double-buffered mode consumes nearly all the RAM available on the
+// Arduino Uno -- only a handful of free bytes remain.  Even the
+// following string needs to go in PROGMEM:
+
+// Similar to F(), but for PROGMEM string pointers rather than literals
+#define F2(progmem_ptr) (const __FlashStringHelper *)progmem_ptr
+
+const char str[] PROGMEM = "Welcome to Computer Science ... ";
+int16_t    textX         = matrix.width(),
+           textMin       = sizeof(str) * -12,
+           hue           = 0;
+
+void setup() {
+  matrix.begin();
+  matrix.setTextWrap(false); // Allow text to run off right edge
+  matrix.setTextSize(2);
+}
+
+void loop() {
+  byte i;
+
+  // Clear background
+  matrix.fillScreen(0);
+
+  // Draw big scrolly text on top
+  matrix.setTextColor(matrix.ColorHSV(hue, 255, 255, true));
+  matrix.setCursor(textX, 1);
+  matrix.print(F2(str));
+
+  // Move text left (w/wrap), increase hue
+  if((--textX) < textMin) textX = matrix.width();
+  hue += 7;
+  if(hue >= 1536) hue -= 1536;
+
+  delay(50);
+  // Update display
+  matrix.swapBuffers(false);
+}
